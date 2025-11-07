a622afccf0c6ad77067cc7ea8a69a0c9ae3fafc2
George Ogden <george-ogden@outlook.com>
2025-11-07 21:17:02 +0000

    Add integration tests

---
+++ integration_tests/frozendict_dict/test_write_change.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+DIRECTORY=$(basename $(dirname $0))
+LOG=`mktemp`
+
+python main.py --fix test_data/$DIRECTORY/{{,no_}errors,combined}.py --fix | tee $LOG && exit 1
+grep -F "11 fixes applied" $LOG
+diff test_data/$DIRECTORY/{errors.py,expected.py}
+diff test_data/$DIRECTORY/combined{,_expected}.py
+git diff --quiet test_data/$DIRECTORY/no_errors.py
