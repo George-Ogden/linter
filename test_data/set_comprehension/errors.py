5eda899133dce95fe7140b16d34fcb8ac1f22e63
George Ogden <george-ogden@outlook.com>
2026-02-01 14:51:15 +0000

    fixup! Delete set-comprehension rule

--- test_data/set_comprehension/errors.py
+++
@@ -1,9 +0,0 @@
-flat = set(x for x in range(5))
-
-bracketed = set((y for y in range(6)))  # noqa: UP034
-
-multiline = set(
-    y + 1 for y in [1, 2, 3]
-)
-
-nested_set_comp = list(set({z for z in range(len(set(u for u in range(6))))}))
